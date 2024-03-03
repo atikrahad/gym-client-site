158df7fdc1b92c5f57a5ec2ad61f26a9f0a8fbee
unknown <atikrahad1@gmail.com>
2024-03-03 16:35:49 +0600

    create register page design with responsive

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,91 @@
+import { NavLink } from "react-router-dom";
+import img from "../../assets/Login/img.jpg";
+import { GoEye, GoEyeClosed } from "react-icons/go";
+import { useState } from "react";
+const Register = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(24,24,24,0.75), rgba(24,24,24,0.85)), url(${img})`,
+      }}
+      className="w-[100vw] h-[100vh] bg-cover"
+    >
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl text-white font-bold">Register now!</h1>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-[#413c3c91]">
+            <form className="card-body ">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-slate-300">Full Name</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="name"
+                  className="input input-bordered"
+                  name="name"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-slate-300">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  name="email"
+                  required
+                />
+              </div>
+              <div className="form-control relative">
+                <label className="label">
+                  <span className="label-text text-slate-300">Password</span>
+                </label>
+                <input
+                  type={`${isOpen ? "text" : "password"}`}
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                  required
+                />
+                <h1
+                  className="absolute cursor-pointer top-12 right-2"
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  {isOpen ? (
+                    <GoEyeClosed className="text-2xl"></GoEyeClosed>
+                  ) : (
+                    <GoEye className="text-2xl"></GoEye>
+                  )}
+                </h1>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-slate-300">Profile Pic</span>
+                </label>
+                <input
+                  type="file"
+                  className="file-input file-input-bordered w-full max-w-xs"
+                />
+              </div>
+
+              <div className="form-control mt-6">
+                <button className="btn-bg">Register</button>
+              </div>
+              <h1 className="text-slate-300">
+                Allready have account? <NavLink to="/login">Login</NavLink>
+              </h1>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home";
 import Main from "../Layout/Main";
 import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 export const router = createBrowserRouter([
     {
@@ -17,5 +18,9 @@ export const router = createBrowserRouter([
     {
       path: "/login",
       element: <Login></Login>
+    },
+    {
+      path: "/register",
+      element: <Register></Register>
     }
   ]);
