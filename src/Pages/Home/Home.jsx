db97915df754f7ce38ca02856e75d69ab5850f69
unknown <atikrahad1@gmail.com>
2024-03-03 12:21:12 +0600

    create shared banner section and added in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,26 @@
+import Banner from "../../Shared/Components/Banner";
 import About from "./homeComponents/About";
 import Classes from "./homeComponents/Classes";
 import Homebenner from "./homeComponents/Homebenner";
 import Status from "./homeComponents/Status";
+import bannerimg from '../../assets/Home/banner/maxresdefault.jpg'
 
 const Home = () => {
-    return (
-        <div>
-            <Homebenner></Homebenner>
-            <About></About>
-            <Status></Status>
-            <Classes></Classes>
-        </div>
-    );
+  return (
+    <div>
+      <Homebenner></Homebenner>
+      <About></About>
+      <Status></Status>
+      <Classes></Classes>
+      <Banner
+        subTitle={"Join our membership"}
+        title={"Let's start healthy journy with us"}
+        btn={'Join Membership'}
+        img={bannerimg}
+        para={'Unlock exclusive benefits and join a community of like-minded individuals by becoming a member today. Gain access to premium content, special offers, member-only events, and personalized support. Experience the full potential of our community and take your journey to the next level with our membership program.'}
+      ></Banner>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Shared/Components/Banner.jsx
@@ -0,0 +1,24 @@
+import { BiDumbbell } from "react-icons/bi";
+
+
+const Banner = ({subTitle,title,para,img, btn}) => {
+  return (
+    <div
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(25, 0 , 0, .69), rgba(0, 2, 25, .69)), url(${img})`,
+      }}
+      className={`py-5 md:h-[500px] flex items-center justify-center bg-cover bg-no-repeat max-w-screen-2xl mx-auto`}
+    >
+        <div className="flex space-y-3 w-full md:w-[40%] mx-auto flex-col items-center">
+            <h3 className="text-2xl text-slate-300">{subTitle}</h3>
+            <h1 className="text-white text-4xl">{title}</h1>
+            <p className="text-slate-400 text-center">{para}</p>
+            {
+                btn ? <button className="btn-bg">{btn}</button>: <BiDumbbell className="text-7xl main-color"></BiDumbbell>
+            }
+        </div>
+    </div>
+  );
+};
+
+export default Banner;
