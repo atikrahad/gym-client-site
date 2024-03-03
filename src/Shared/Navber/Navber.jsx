9ac49fa4c52dbba08f30fef910454112517e28d4
unknown <atikrahad1@gmail.com>
2024-03-03 18:18:44 +0600

    adding stickyNav ber function

--- src/Shared/Navber/Navber.jsx
+++ src/Shared/Navber/Navber.jsx
@@ -2,10 +2,34 @@ import { NavLink } from "react-router-dom";
 import img from "../../assets/logo/gymlogo.png";
 import Navlinks from "./NavComponents/Navlinks";
 import Navmenu from "./NavComponents/Navmenu";
+import { useEffect, useState } from "react";
 const Navber = () => {
+  const [stickyclass, setStickyclass] = useState("")
+  const [shadow,setShadow] = useState('none')
+  useEffect(()=>{
+    window.addEventListener('scroll', fixdNav);
+    return ()=> {
+      window.removeEventListener("scroll", fixdNav)
+    }
+  },[])
+
+  const fixdNav = ()=> {
+    if(window !== undefined){
+      let windowHight = window.scrollY;
+      if(windowHight > 70){
+        setStickyclass('bg-slate-900  top-0 left-0 bg-opacity-60')
+        setShadow("2px 4px 16px #2e5354")
+      }
+      else{
+        setStickyclass('fixed')
+        setShadow("0px 0px 0px #2e5354")
+      }
+    }
+  }
+
   return (
     <div
-      className=" fixed z-30 w-full py-2  mx-auto "
+      className= {`fixed z-30 py-5 ${stickyclass} ${shadow} w-full py-2  mx-auto `}
     >
       <div className="flex max-w-screen-xl mx-auto items-center justify-between">
         <div className="flex items-center">
