ecd3a16063a3b7e419c8e74a26ac637a3b1f4a97
unknown <atikrahad1@gmail.com>
2024-03-08 16:07:04 +0600

    adding some linkup with homepage

--- src/Pages/Home/homeComponents/Homebenner.jsx
+++ src/Pages/Home/homeComponents/Homebenner.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
+import { NavLink } from "react-router-dom";
+import useAuth from "../../../Contextapi/useAuth";
 import banner from "../../../assets/Home/banner/banner.mp4";
 const Homebenner = () => {
+  const {user} = useAuth()
   return (
     <div className="relative flex items-center justify-center max-w-screen-2xl overflow-hidden md:h-[440px] lg:h-[600px] mx-auto">
       <div className="gradient-overlay z-10"></div>
@@ -11,7 +14,9 @@ const Homebenner = () => {
         <p className="text-center text-xl md:text-3xl text-white">Welcome To ARGym</p>
         <h1 className="text-2xl font-bold md:w-[60%] md:text-5xl text-center text-red-50">It's Your Time To Gain More Healthy and Muscles</h1>
         <p className="text-center md:w-[60%] text-white">Elevate your fitness journey in our state-of-the-art gym, featuring cutting-edge equipment, diverse workout classes, and expert trainers dedicated to your health and wellness.</p>
-        <button className="btn-bg">Get Started</button>
+        {
+          !user && <NavLink to="/login"><button  className="btn-bg">Get Started</button></NavLink> 
+        }
         </div>
       </div>
     </div>
--- src/Shared/Components/Banner.jsx
+++ src/Shared/Components/Banner.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/prop-types */
 import { BiDumbbell } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 
 const Banner = ({subTitle,title,para,img, btn}) => {
@@ -14,7 +16,7 @@ const Banner = ({subTitle,title,para,img, btn}) => {
             <h1 className="text-white text-center font-bold text-5xl">{title}</h1>
             <p className="text-slate-400 font-medium text-xl text-center">{para}</p>
             {
-                btn ? <button className="btn-bg">{btn}</button>: <BiDumbbell className="text-7xl main-color"></BiDumbbell>
+                btn ? <Link to="/plan"><button className="btn-bg">{btn}</button></Link>: <BiDumbbell className="text-7xl main-color"></BiDumbbell>
             }
         </div>
     </div>
